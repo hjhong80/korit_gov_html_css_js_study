cef29e9a4187bdf3b4087d300e3e9c8768b1ccef
hjhong80 <ghdgowns@gmail.com>
2025-12-17 16:44:38 +0900

    feat : todo_practice final

--- todo_list/index.js
+++ todo_list/index.js
@@ -1,7 +1,11 @@
 const addBtn = document.querySelector("#input_btn_box > button");
-const addTopoInput = document.querySelector("#input_btn_box > input");
+const addTodoInput = document.querySelector("#input_btn_box > input");
+const todoListUl = document.querySelector("#todo_list");
+const modal = document.querySelector("#modal_container");
+const modalInput = document.querySelector("#modal_input_box > input");
 
 let todoList = [];
+let editTargetTodoId = 0;
 
 window.onload = () => {
     if (!localStorage.getItem("TodoList")) {
@@ -9,20 +13,20 @@ window.onload = () => {
     } else {
         todoList = JSON.parse(localStorage.getItem("TodoList"));
     }
+
+    todoRender();
 };
 
 function addTodoBtnOnclick() {
-    if (addTopoInput.value === "") {
+    if (addTodoInput.value === "") {
         alert("할 일을 입력해주세요.");
         return;
     }
-
-    todoList = localStorage.getItem("TodoList");
     let todoId = 0;
-
     if (todoList.length !== 0) {
         todoId = todoList[todoList.length - 1].id + 1;
     }
+
     let today = new Date();
 
     let year = today.getFullYear();
@@ -33,26 +37,154 @@ function addTodoBtnOnclick() {
 
     const newTodo = {
         id: todoId,
-        content: addTopoInput.value,
-        date: dateString,
+        content: addTodoInput.value,
         complete: false,
+        date: dateString,
     };
 
     todoList = [...todoList, newTodo];
     localStorage.setItem("TodoList", JSON.stringify(todoList));
 
-    addTopoInput.value = "";
-    alert("할 일이 등록 되었습니다.");
+    addTodoInput.value = "";
+
+    alert("할 일이 등록되었습니다.");
+
+    todoRender();
 }
 
 addBtn.addEventListener("click", addTodoBtnOnclick);
 
-function todoRender() {
+function dateGroup() {
     todoList = JSON.parse(localStorage.getItem("TodoList"));
+    const group = {};
+    for (const todo of todoList) {
+        if (!group[todo.date]) {
+            group[todo.date] = [];
+        }
+        group[todo.date].push(todo);
+    }
+    return group;
+}
 
-    const todoListHtml = todoList.map((todo) => {
-        return `
-        
-        `;
+function todoRender() {
+    const groupTodo = dateGroup();
+
+    const dateKeys = Object.keys(groupTodo);
+
+    todoListUl.innerHTML = dateKeys
+        .map((dateKey) => {
+            const todo = groupTodo[dateKey];
+            return `
+                <li>
+                    <div class="todo_box">
+                        <div class="todo_date">${dateKey}</div>
+                        ${todo
+                            .map((t) => {
+                                return `
+                                    <div class="todo">
+                                            <div class="control-container">
+                                                <input
+                                                    type="checkbox"
+                                                    id="todo_${t.id}"
+                                                    class="screen-reader"
+                                                    onclick="checkClick(${
+                                                        t.id
+                                                    })"
+                                                    ${
+                                                        t.complete
+                                                            ? "checked"
+                                                            : ""
+                                                    }
+                                                />
+                                                <div class="label-box">
+                                                    <span
+                                                        class="check-icon"
+                                                        aria-hidden="true">
+                                                    </span>
+                                                    <label for="todo_${t.id}">
+                                                        ${t.content}
+                                                    </label>
+                                                </div>
+                                            </div>
+                                            <div class="todo_btn_box">
+                                                <button onclick="delButtonClick(${
+                                                    t.id
+                                                })">
+                                                    삭제
+                                                </button>
+                                                <button onclick="openModalClick(${
+                                                    t.id
+                                                })">수정</button>
+                                            </div>
+                                        </div>
+                                `;
+                            })
+                            .join("")}
+                    </div>
+                </li>
+            `;
+        })
+        .join("");
+}
+
+function checkClick(todoId) {
+    todoList = JSON.parse(localStorage.getItem("TodoList"));
+    todoList = todoList.map((todo) => {
+        if (todo.id === todoId) {
+            todo.complete = !todo.complete;
+        }
+        return todo;
     });
+
+    localStorage.setItem("TodoList", JSON.stringify(todoList));
+
+    todoRender();
+}
+
+function delButtonClick(todoId) {
+    if (!confirm("정말로 삭제하시겠습니까?")) {
+        return;
+    }
+    todoList = JSON.parse(localStorage.getItem("TodoList"));
+    todoList = todoList.filter((todo) => todo.id !== todoId);
+
+    localStorage.setItem("TodoList", JSON.stringify(todoList));
+    todoRender();
+}
+
+function editClick() {
+    if (modalInput.value === "") {
+        alert("수정할 내용을 입력해주세요");
+        return;
+    }
+
+    if (!confirm("수정하시겠습니까?")) {
+        return;
+    }
+
+    todoList = JSON.parse(localStorage.getItem("TodoList"));
+    todoList = todoList.map((todo) => {
+        if (todo.id === editTargetTodoId) {
+            todo.content = modalInput.value;
+        }
+        return todo;
+    });
+
+    localStorage.setItem("TodoList", JSON.stringify(todoList));
+
+    todoRender();
+
+    cancelClick();
+}
+
+function openModalClick(todoId) {
+    modal.style.display = "flex";
+    editTargetTodoId = todoId;
+    todoList = JSON.parse(localStorage.getItem("TodoList"));
+    todo = todoList.find((todo) => todo.id === todoId);
+    modalInput.value = todo.content;
+}
+
+function cancelClick() {
+    modal.style.display = "none";
 }
